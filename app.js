fa6c831e919b2afe5f6fa3083b25d4c9801ce1ff
jlopez-duffion <jordan.lopez0117@gmail.com>
2025-07-30 15:32:34 -0700

    Waited for DOM content to load

--- app.js
+++ app.js
@@ -1,25 +1,29 @@
-const formElem = document.getElementById('raidForm');
-const submitBtn = document.getElementById('codeSubmit');
-const outputElem = document.querySelector('.decrypted-code-output');
+document.addEventListener("DOMContentLoaded", (event) => {
 
-formElem.addEventListener("submit", function(event) {
-  
-  event.preventDefault();
-  
-  let formData = new FormData(formElem, submitBtn);
-  const raidCode = formData.get('code');
-  
-  let splitCode = raidCode.split('');
-  let newCode = [];
-  newCode.push(splitCode.shift());
-  newCode.push(splitCode.pop());
-  newCode.push(splitCode.shift());
-  newCode.push(splitCode.pop());
-  newCode.push(splitCode.shift());
-  newCode.push(splitCode.pop());
-  
-  let decrypted = newCode.join('');
-  
-  outputElem.textContent = decrypted;
-  
+    const formElem = document.getElementById('raidForm');
+    const submitBtn = document.getElementById('codeSubmit');
+    const outputElem = document.querySelector('.decrypted-code-output');
+
+    formElem.addEventListener("submit", function(event) {
+    
+    event.preventDefault();
+    
+    let formData = new FormData(formElem, submitBtn);
+    const raidCode = formData.get('code');
+    
+    let splitCode = raidCode.split('');
+    let newCode = [];
+    newCode.push(splitCode.shift());
+    newCode.push(splitCode.pop());
+    newCode.push(splitCode.shift());
+    newCode.push(splitCode.pop());
+    newCode.push(splitCode.shift());
+    newCode.push(splitCode.pop());
+    
+    let decrypted = newCode.join('');
+    
+    outputElem.textContent = decrypted;
+    
+    });
+
 });
