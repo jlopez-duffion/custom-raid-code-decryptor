9d2b6dee706faa08027f9521fc24dc439bc5863c
jlopez-duffion <jordan.lopez0117@gmail.com>
2025-07-30 23:30:33 -0700

    Added crypt key feature to dynamically change raid encryption on the fly

--- app.js
+++ app.js
@@ -1,29 +1,41 @@
 document.addEventListener("DOMContentLoaded", (event) => {
 
     const formElem = document.getElementById('raidForm');
-    const submitBtn = document.getElementById('codeSubmit');
+    const submitBtn = document.getElementById('formSubmit');
     const outputElem = document.querySelector('.decrypted-code-output');
 
     formElem.addEventListener("submit", function(event) {
-    
+
     event.preventDefault();
-    
+
     let formData = new FormData(formElem, submitBtn);
     const raidCode = formData.get('code');
-    
+    const cryptKey = formData.get('key');
+
     let splitCode = raidCode.split('');
-    let newCode = [];
-    newCode.push(splitCode.shift());
-    newCode.push(splitCode.pop());
-    newCode.push(splitCode.shift());
-    newCode.push(splitCode.pop());
-    newCode.push(splitCode.shift());
-    newCode.push(splitCode.pop());
-    
-    let decrypted = newCode.join('');
-    
+    let splitKey = cryptKey.split('');
+    let entriesMap = [
+        [splitKey[0],splitCode[0]],
+        [splitKey[1],splitCode[1]],
+        [splitKey[2],splitCode[2]],
+        [splitKey[3],splitCode[3]],
+        [splitKey[4],splitCode[4]],
+        [splitKey[5],splitCode[5]],
+    ];
+
+    let sorted = Object.fromEntries(entriesMap);
+
+    let decrypted = [
+        sorted["1"],
+        sorted["2"],
+        sorted["3"],
+        sorted["4"],
+        sorted["5"],
+        sorted["6"],
+    ].join("");
+
     outputElem.textContent = decrypted;
-    
+
     });
 
 });
